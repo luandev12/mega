43ee4307020d56125cbc5c5709a352772e31adcd
luandevpro <luandevpro@gmail.com>
2021-09-01 10:28:59 +0700

    v1.0.4

---
+++ src/intergations/firebase.tsx
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAa9YACFwWSWt4tJVeWwBXx-0i0LreL70Q',
+  authDomain: 'nemo-mega.firebaseapp.com',
+  projectId: 'nemo-mega',
+  storageBucket: 'nemo-mega.appspot.com',
+  messagingSenderId: '563076820236',
+  appId: '1:563076820236:web:23e55aa37073d5ec6bad29',
+  measurementId: 'G-T9RDWNYG8M',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+// https://firebase.google.com/docs/storage/web/start
+export const storage = getStorage(app);
+// https://firebase.google.com/docs/firestore/quickstart#web-version-9_1
+export const db = getFirestore();
+// https://firebase.google.com/docs/auth/web/start
+export const auth = getAuth();
