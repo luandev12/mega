1fc93967654f0266c409a89dc4f870da30898e1a
luandevpro <luandevpro@gmail.com>
2021-09-01 09:38:59 +0700

    v1.0.2

--- .umirc.ts
+++ .umirc.ts
@@ -1,36 +1,35 @@
 import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
   routes: [
     {
       path: '/',
       component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+      routes: [{ path: '/', component: '../pages/index' }],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: false,
-      dynamicImport: { webpackChunkName: true },
-      title: 'nemo-mega',
-      dll: false,
-      locale: {
-        enable: true,
-        default: 'en-US',
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: false,
+        dynamicImport: { webpackChunkName: true },
+        title: 'nemo-mega',
+        dll: false,
+        locale: {
+          enable: true,
+          default: 'en-US',
+        },
+        routes: {
+          exclude: [/components\//],
+        },
       },
-      routes: {
-        exclude: [
-          /components\//,
-        ],
-      },
-    }],
+    ],
   ],
-}
+};
 
 export default config;
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './index.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 const BasicLayout: React.FC = props => {
   return <div className={styles.normal}>{props.children}</div>;
 };
